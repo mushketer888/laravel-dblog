8fa3331e4a3463edc5d93d35b90e946df66dbd5a
Ilja <mushketer888@gmail.com>
2017-08-06 04:27:04 +0300

    Cleaning

--- src/Writer.php
+++ src/Writer.php
@@ -2,22 +2,8 @@
 
 namespace Mushketer888\LaravelDblog;
 
-use Illuminate\Support\Facades\Log;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\SyslogHandler;
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\ErrorLogHandler;
-use Monolog\Logger as MonologLogger;
-use Monolog\Handler\RotatingFileHandler;
-use Illuminate\Contracts\Support\Jsonable;
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Contracts\Support\Arrayable;
-use Psr\Log\LoggerInterface as PsrLoggerInterface;
-use Illuminate\Contracts\Logging\Log as LogContract;
-
 class Writer extends \Illuminate\Log\Writer
 {
-
 
     protected function writeLog($level, $message, $context)
     {
